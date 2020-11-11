c8a8798940543d5935a02d249c5a10f06efcab73
Shuntao Chen <359000081@qq.com>
2020-11-11 11:16:59 +0800

    add time stamp for each added new content section

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ function main() {
     app.post('/md', function (req, res, next) {
         if (!checkVipOrAdmin(req)) return
         const { cate, name, content } = req.body
-        writeMdFile(cate, name, content + '\n\n' + "<font color='red'> Last edit: " + new Date().toString() + "</font>")
+        writeMdFile(cate, name, content + '\n\n' + "<font color='purple'> Last edit: " + new Date().toString() + "</font>")
         refreshMdsCache()
         res.send('OK')
 
